ad795c5d23e34a75a8f85668bd819b9f1337af0e
legendary1205 <sajjadrainbow1605@outlook.com>
2025-12-12 04:05:27 +0330

    Initial commit

--- install.sh
+++ install.sh
@@ -1,99 +1,154 @@
 #!/bin/bash
 
-# ------------------- COLORS --------------------
-CYAN="\e[96m"
-NC="\e[0m"
+##############################################
+#   R A P I D O   S E R V E R   (CYAN MENU)  #
+##############################################
 
-# ------------------- MENU ----------------------
 clear
-echo -e "${CYAN}"
-echo -e "██████╗  █████╗ ██████╗ ██╗██╗  ██████╗  ██████╗     ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ "
-echo -e "██╔══██╗██╔══██╗██╔══██╗██║██║  ██╔══██╗██╔════╝     ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗"
-echo -e "██████╔╝███████║██████╔╝██║██║  ██████╔╝██║  ███╗    ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝"
-echo -e "██╔══██╗██╔══██║██╔══██╗██║██║  ██╔══██╗██║   ██║    ╚════██║██╔══╝  ██╔══██╗██║   ██║██╔══╝  ██╔══██╗"
-echo -e "██████╔╝██║  ██║██║  ██║██║██║  ██║  ██║╚██████╔╝    ███████║███████╗██║  ██║╚██████╔╝███████╗██║  ██║"
-echo -e "╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝  ╚═╝ ╚═════╝     ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝"
-echo -e "${NC}"
+echo -e "\e[96m"
+echo "██████╗  █████╗ ██████╗ ██╗██╗   ██╗██████╗  █████╗ "
+echo "██╔══██╗██╔══██╗██╔══██╗██║██║   ██║██╔══██╗██╔══██╗"
+echo "██████╔╝███████║██████╔╝██║██║   ██║██████╔╝███████║"
+echo "██╔══██╗██╔══██║██╔══██╗██║╚██╗ ██╔╝██╔══██╗██╔══██║"
+echo "██████╔╝██║  ██║██║  ██║██║ ╚████╔╝ ██████╔╝██║  ██║"
+echo "╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═════╝ ╚═╝  ╚═╝"
+echo -e "\e[0m"
 
 echo ""
-echo "1) Install Mikrotik Automatic"
-echo "2) Install Mikrotik Custom"
+echo "1) Install MikroTik (Automatic)"
+echo "2) Install MikroTik (Custom)"
 echo "3) Exit"
 echo ""
-read -p "Select an option (1/2/3): " menu
 
-# ------------------- FUNCTIONS ----------------------
+read -p "Select option [1-3]: " MENU
 
-install_mikrotik() {
-    local VERSION=$1
+##############################################
+# FUNCTIONS
+##############################################
 
+ask_interface() {
     echo ""
-    read -p "Enter network interface name (ex: eth0): " NETIF
-    read -p "Enter disk name (ex: vda or sda): " DISK
-
-    ADDRESS="ip addr show $NETIF | grep global | cut -d' ' -f 6 | head -n 1"
-    GATEWAY="ip route list | grep default | cut -d' ' -f 3"
-
+    echo "Available interfaces:"
+    ip link | awk -F': ' '/^[0-9]+:/{print " - " $2}'
     echo ""
-    echo "-----------------------------------------"
-    echo "Network Interface: $NETIF"
-    echo "Disk: $DISK"
-    echo "Version: $VERSION"
-    echo "-----------------------------------------"
-    read -p "Are you sure to install Mikrotik? (yes/no): " CONFIRM
+    read -p "Enter your Network Interface (e.g. eth0, ens3): " IFACE
+}
 
-    if [[ "$CONFIRM" != "yes" ]]; then
+ask_disk() {
+    echo ""
+    echo "Available disks:"
+    lsblk -d -o NAME,SIZE
+    echo ""
+    read -p "Enter your Disk Name (e.g. vda, sda): " DISK
+}
+
+confirm() {
+    echo ""
+    read -p "Are you sure you want to install MikroTik? (yes/no): " ANSW
+    if [[ "$ANSW" != "yes" ]]; then
         echo "Installation canceled."
         exit 0
     fi
+}
 
-    # ------------------- INSTALLATION ----------------------
+##############################################
+# OPTION 3 → EXIT
+##############################################
+
+if [[ "$MENU" == "3" ]]; then
+    echo "با موفقیت خارج شدید"
+    exit 0
+fi
+
+##############################################
+# OPTION 1 → AUTOMATIC
+##############################################
+
+if [[ "$MENU" == "1" ]]; then
+    ask_interface
+    ask_disk
+
+    VERSIONCHR="7.19.3"
+
+    ADDRESS="ip addr show $IFACE | grep global | cut -d' ' -f 6 | head -n 1"
+    GATEWAY="ip route list | grep default | cut -d' ' -f 3"
+
+    confirm
+
+    # =========================================
+    # START INSTALLATION
+    # =========================================
+    eval ADDRESS=$ADDRESS
+    eval GATEWAY=$GATEWAY
 
     apt update
     apt install -y pwgen coreutils unzip
-
-    wget -4 https://download.mikrotik.com/routeros/$VERSION/chr-$VERSION.img.zip -O chr.img.zip
+    wget -4 https://download.mikrotik.com/routeros/$VERSIONCHR/chr-$VERSIONCHR.img.zip -O chr.img.zip
     gunzip -c chr.img.zip > chr.img
-
     mount -o loop,offset=33571840 chr.img /mnt
 
     PASSWORD=$(pwgen 12 1)
+
     echo "Username: admin"
     echo "Password: $PASSWORD"
 
-    echo "/ip address add address=\$($ADDRESS) interface=[/interface ethernet find where name=ether1]" > /mnt/rw/autorun.scr
-    echo "/ip route add gateway=\$($GATEWAY)" >> /mnt/rw/autorun.scr
+    echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]" > /mnt/rw/autorun.scr
+    echo "/ip route add gateway=$GATEWAY" >> /mnt/rw/autorun.scr
     echo "/ip service disable telnet" >> /mnt/rw/autorun.scr
     echo "/user set 0 name=admin password=$PASSWORD" >> /mnt/rw/autorun.scr
     echo "/ip dns set server=8.8.8.8,1.1.1.1" >> /mnt/rw/autorun.scr
 
-    echo u > /proc/sysrq-trigger
-    dd if=chr.img bs=1024 of=/dev/$DISK
+    # *** DANGEROUS – Uncomment manually ***
+    # echo u > /proc/sysrq-trigger
+    # dd if=chr.img bs=1024 of=/dev/$DISK
+    # echo s > /proc/sysrq-trigger
+    # echo b > /proc/sysrq-trigger
 
-    echo "sync disk"
-    echo s > /proc/sysrq-trigger
-
-    echo "Rebooting in 10 seconds..."
-    read -t 10 -u 1
-    echo b > /proc/sysrq-trigger
-}
-
-# ------------------- MENU HANDLER ----------------------
-
-case $menu in
-1)
-    install_mikrotik "7.19.3"
-    ;;
-2)
-    read -p "Enter MikroTik version (ex: 7.19.3): " CUSTOM_VERSION
-    install_mikrotik "$CUSTOM_VERSION"
-    ;;
-3)
-    echo "با موفقیت خارج شدید."
     exit 0
-    ;;
-*)
-    echo "Invalid option!"
-    exit 1
-    ;;
-esac
+fi
+
+##############################################
+# OPTION 2 → CUSTOM
+##############################################
+
+if [[ "$MENU" == "2" ]]; then
+    ask_interface
+    ask_disk
+    read -p "Enter MikroTik version (example: 7.19.3): " VERSIONCHR
+
+    ADDRESS="ip addr show $IFACE | grep global | cut -d' ' -f 6 | head -n 1"
+    GATEWAY="ip route list | grep default | cut -d' ' -f 3"
+
+    confirm
+
+    # =========================================
+    # START INSTALLATION
+    # =========================================
+    eval ADDRESS=$ADDRESS
+    eval GATEWAY=$GATEWAY
+
+    apt update
+    apt install -y pwgen coreutils unzip
+    wget -4 https://download.mikrotik.com/routeros/$VERSIONCHR/chr-$VERSIONCHR.img.zip -O chr.img.zip
+    gunzip -c chr.img.zip > chr.img
+    mount -o loop,offset=33571840 chr.img /mnt
+
+    PASSWORD=$(pwgen 12 1)
+
+    echo "Username: admin"
+    echo "Password: $PASSWORD"
+
+    echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]" > /mnt/rw/autorun.scr
+    echo "/ip route add gateway=$GATEWAY" >> /mnt/rw/autorun.scr
+    echo "/ip service disable telnet" >> /mnt/rw/autorun.scr
+    echo "/user set 0 name=admin password=$PASSWORD" >> /mnt/rw/autorun.scr
+    echo "/ip dns set server=8.8.8.8,1.1.1.1" >> /mnt/rw/autorun.scr
+
+    # *** DANGEROUS – Uncomment manually ***
+    # echo u > /proc/sysrq-trigger
+    # dd if=chr.img bs=1024 of=/dev/$DISK
+    # echo s > /proc/sysrq-trigger
+    # echo b > /proc/sysrq-trigger
+
+    exit 0
+fi
